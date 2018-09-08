fc1b3140d61085de24ec901cdebd6770f1552eb7
kamgaurav83@gmail.com <Administrator@gauravkam.fareast.corp.microsoft.com>
2018-09-08 06:48:56 +0530

    commit

--- UniversalDashboard/1.0.275/netcoreapp2.0/Resources/Components/Grid.js
+++
@@ -1,8 +0,0 @@
-﻿var datatable = $('#{componentName}').DataTable({ "ajax": "/component/datatable/{componentName}", "serverSide": true, "columns": [{columns}], ordering: {disableSort} });
-
-if ({autoRefresh}) {
-    setInterval(function() {
-            datatable.ajax.reload();
-        },
-        {refreshInterval});
-}
--- cpu_counter_job.ps1
+++
@@ -1,2 +0,0 @@
-﻿Start-Job -ScriptBlock { Get-Counter -Counter “\Processor(_Total)\% Processor Time” -SampleInterval 300 -Continuous | 
-Export-Counter -Path D:\gauravkam\scripts\CPU.csv -Force -FileFormat "csv" }
