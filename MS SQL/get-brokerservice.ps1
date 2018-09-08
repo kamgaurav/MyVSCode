befbabbd8fb42a74514db7b827e3254d42b6cb71
kamgaurav83@gmail.com <Administrator@gauravkam.fareast.corp.microsoft.com>
2018-09-08 06:58:21 +0530

    commit

--- MS SQL/get-brokerservice.ps1
+++
@@ -1,1 +0,0 @@
-﻿Invoke-Sqlcmd -Query "SELECT is_broker_enabled FROM sys.databases WHERE name = 'ETL_Controller';" -ServerInstance "MSLT-319\LEXBI_DI03_16PPE" 
--- MS SQL/set-sqlcredentials_Ver_1.0.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module sqlps -DisableNameChecking
-Set-Location SQLServer:\SQL\gauravkam\default
-$Secret = Read-Host -AsSecureString
-Get-SqlCredential -Name "DomainCredential" | Set-SqlCredential -Identity "fareast\v-gakamb" -Secret $Secret
-
-#Set-SqlCredential -Path 'SQLServer:\SQL\gauravkam\default\Credentials\DomainCredential' -Identity "fareast\v-gakamb" -Secret $Secret
