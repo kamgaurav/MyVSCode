eab5cb1da51ac184ff321bd309bb7328e38527c6
kamgaurav <43080935+kamgaurav@users.noreply.github.com>
2018-09-28 14:42:04 +0530

    Add files via upload

---
+++ Start_Stop_Instance/Start-Stop-EC2.ps1
@@ -0,0 +1,7 @@
+$NewIAMPolicy = New-IAMPolicy -policyname Start-Stop-EC2 -policydocument (Get-Content -raw Start-Stop-EC2.json) -description 'Policy to Start & Stop EC2 Instance'
+$NewIAMRole = New-IAMRole -AssumeRolePolicyDocument (Get-Content -raw Lambda-Assume-Role.json) -RoleName "Lambda-Start-Stop-EC2" -Description 'Allows Lambda Function to call AWS Services'
+Register-IAMRolePolicy -RoleName "Lambda-Start-Stop-EC2" -PolicyArn $NewIAMPolicy.arn
+New-AWSPowerShellLambda -ScriptName EC2InstanceStop -Template Basic
+Publish-AWSPowerShellLambda -ScriptPath .\RDPLockDown\RDPLockDown.ps1 -Name  EC2InstanceStop -Region us-east-1 -IAMRoleArn $NewIAMRole.Arn
+
+
