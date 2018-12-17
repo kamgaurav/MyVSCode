05ffa2914f05af5677bc3cee6ae8232951b57817
kamgaurav83@gmail.com <Administrator@gauravkam.fareast.corp.microsoft.com>
2018-12-18 00:42:11 +0530

    commit

--- Lambda-EBSSnapshot/Remove-EBSSnapshot.ps1
+++ Lambda-EBSSnapshot/Remove-EBSSnapshot.ps1
@@ -1,6 +1,11 @@
-$RetentionDate = ([DateTime]::Now).AddDays(-15)
-Get-EC2Snapshot -Filter @{Name = "tag:BackupType"; Values = "Daily"} | Where-Object { [datetime]::Parse($_.StartTime) -GT $RetentionDate} |
-    ForEach-Object {
-        $SnapshotId = $_.SnapshotId
-        Remove-EC2Snapshot -SnapshotId $SnapshotId -Force
+#Requires -Modules 'AWSPowerShell.NetCore'
+$RetentionDays = 14
+$Snapshots = Get-EC2Snapshot -Filter @{Name = "tag:BackupType"; Values = "Daily"}
+ForEach ($Snapshot in $Snapshots)
+    { 
+    $Retention = ([DateTime]::Now).AddDays(-$RetentionDays)
+    if ([DateTime]::Compare($Retention, $Snapshot.StartTime) -gt 0)
+        { 
+        Remove-EC2Snapshot -SnapshotId $snapshot.SnapshotId -Force 
+        } 
     }
