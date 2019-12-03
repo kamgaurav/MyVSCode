7f9530aa218ed507c5d8f3e746f28cdda1438b5b
kamgaurav <kamgaurav83@gmail.com>
2019-12-03 20:27:04 +0530

    commit

---
+++ PowerShell/FareLogix/Set-EnvVariable.ps1
@@ -0,0 +1,52 @@
+﻿param([switch]$Elevated)
+
+function Set-Env (){
+
+    $AWS_ACCESS_KEY_ID = Read-Host -Prompt "Enter AWS Access Key ID"
+    $AWS_SECRET_ACCESS_KEY = Read-Host -Prompt "Enter AWS Secret Access Key"
+
+    #Set-Item -Path Env:AWS_ACCESS_KEY_ID -Value ($Env:AWS_ACCESS_KEY_ID + "$AWS_ACCESS_KEY_ID")
+    #Set-Item -Path Env:AWS_SECRET_ACCESS_KEY -Value ($Env:AWS_SECRET_ACCESS_KEY + "$AWS_SECRET_ACCESS_KEY")
+
+    [Environment]::SetEnvironmentVariable("AWS_ACCESS_KEY_ID","$AWS_ACCESS_KEY_ID","Machine")
+    [Environment]::SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY","$AWS_SECRET_ACCESS_KEY","Machine")
+    Write-Host ""
+    Write-Host -ForegroundColor Yellow "Environment Variables Added !!"
+    Write-Host ""
+
+}
+
+function Remove-Env (){
+    #Remove-Item -Path Env:AWS_ACCESS_KEY_ID
+    #Remove-Item -Path Env:AWS_SECRET_ACCESS_KEY
+
+    [Environment]::SetEnvironmentVariable("AWS_ACCESS_KEY_ID","","Machine")
+    [Environment]::SetEnvironmentVariable("AWS_SECRET_ACCESS_KEY","","Machine")
+    Write-Host ""
+    Write-Host -ForegroundColor Yellow "Environment Variables Removed !!"
+    Write-Host ""
+}
+
+function Get-Env (){
+    #Get-ChildItem Env:* | Where-Object {$_.Name -eq 'AWS_ACCESS_KEY_ID'}
+    #Get-ChildItem Env:* | Where-Object {$_.Name -eq 'AWS_SECRET_ACCESS_KEY'}
+
+    [Environment]::GetEnvironmentVariable("AWS_ACCESS_KEY_ID", "machine")
+    [Environment]::GetEnvironmentVariable("AWS_SECRET_ACCESS_KEY", "machine")
+}
+
+While(($Case = Read-Host -Prompt "Set or Remove Environment Variables") -ne "q"){
+
+Switch($Case){
+        s { Set-Env }
+        r { Remove-Env }
+        g { Get-Env }
+        q { "End" }
+        default { 
+        Write-Host ""
+        Write-Host -ForegroundColor Red "Invalid entry: Please enter Set(s) or Remove(r) !!!" 
+        Write-Host ""
+        }
+    }
+}
+
---
+++ PowerShell/FareLogix/Set-Net_Ver-2.0.ps1
@@ -0,0 +1,56 @@
+﻿function Set-Proxy { 
+    [CmdletBinding()]
+    [Alias('proxy')]
+    [OutputType([string])]
+    Param
+    (
+        # server address
+        [Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0)]
+        $server,
+        # port number
+        [Parameter(Mandatory = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 1)]
+        $port    
+    )
+    
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyServer -Value "$($server):$($port)"
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyEnable -Value 1
+         
+        
+}
+ 
+
+function Remove-Proxy (){    
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyServer -Value ""
+    Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings' -name ProxyEnable -Value 0
+}
+
+
+function Get-Proxy (){
+    $a = Get-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings'
+    $a.ProxyServer
+    $a.ProxyEnable
+}
+
+
+While(($Case = Read-Host -Prompt "Set or Remove") -ne "q"){
+
+Switch($Case){
+        s { Set-Proxy 172.28.172.20 8080 }
+        r { Remove-Proxy }
+        g { Get-Proxy }
+        q { "End" }
+        default { 
+        Write-Host ""
+        Write-Host -ForegroundColor Red "Invalid entry: Please enter Set(s) or Remove(r) !!!" 
+        Write-Host ""
+        }
+    }
+}
+
+Start-Process ms-settings:network-proxy 
+Start-Sleep -Milliseconds 100
+Stop-Process -Name SystemSettings
